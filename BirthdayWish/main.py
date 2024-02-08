439d7c613ba831ca5796efccb9c516d9fbdef815
aprilcao221 <97302206+aprilcao221@users.noreply.github.com>
2024-02-08 12:48:57 +0800

    Update main.py

--- BirthdayWish/main.py
+++ BirthdayWish/main.py
@@ -14,7 +14,7 @@ birthday_today = [person for person in birthdays if person["month"] == month and
 for person in birthday_today:
     with open(f"letter_templates/letter_{randint(1,3)}.txt") as letter:
         content = letter.read()
-        new_content = content.replace("[NAME]", f"{person["name"]}")
+        new_content = content.replace("[NAME]", f"{person['name']}")
     with smtplib.SMTP("smtp.163.com", 25) as connection:
         connection.starttls()
         connection.login(user="caomancomeon@163.com", password="TJWJFXASKQGBHELS")
