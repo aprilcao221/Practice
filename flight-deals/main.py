409176fef649384c7adafb03c8c12ef80a0d2773
aprilcao221 <97302206+aprilcao221@users.noreply.github.com>
2024-02-16 14:02:13 +0800

    updates

--- flight-deals/main.py
+++ flight-deals/main.py
@@ -2,13 +2,9 @@ from data_manager import DataManager
 from flight_search import FlightSearch
 from flight_data import FlightData
 from notification_manager import NotificationManager
-from datetime import datetime
-from dateutil.relativedelta import relativedelta
-from pprint import pprint
 
 flight_search = FlightSearch()
 data_manager = DataManager()
-pprint(data_manager.user_data)
 
 
 if data_manager.user_data == "":
@@ -23,6 +19,7 @@ for city in data_manager.user_data:
                    f"GBP{flight_data.price} "
                    f"for flight number {flight_data.to_flight_no} and flight number {flight_data.return_flight_no}"
                    f"leaving on {flight_data.leaving_on.split('T')[0]} and return on {flight_data.return_on.split('T')[0]}")
+        NotificationManager(message)
 
 
 
--- flight-deals/notification_manager.py
+++ flight-deals/notification_manager.py
@@ -1,3 +1,12 @@
+from twilio.rest import Client
+import os
+
+
 class NotificationManager:
-    #This class is responsible for sending notifications with the deal flight details.
-    pass
+    def __init__(self, body):
+        self.client = Client(os.environ['account_sid'], os.environ['auth_token'])
+        self.client.messages.create(
+            from_='+18329240661',
+            to='+8613128840500',
+            body=body
+        )
