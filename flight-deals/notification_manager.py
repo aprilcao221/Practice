5555f5ba96a4a401149c96d5d94b40828b56a45c
aprilcao221 <97302206+aprilcao221@users.noreply.github.com>
2024-02-16 14:15:59 +0800

    Update notification_manager.py

--- flight-deals/notification_manager.py
+++ flight-deals/notification_manager.py
@@ -5,8 +5,10 @@ import os
 class NotificationManager:
     def __init__(self, body):
         self.client = Client(os.environ['account_sid'], os.environ['auth_token'])
-        self.client.messages.create(
-            from_='+18329240661',
-            to='+8613128840500',
+        message = self.client.messages.create(
+            from_=os.environ['from'],
+            to=os.environ['to'],
             body=body
         )
+        print(message.sid)
+
