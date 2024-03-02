9386532a3c97ed7f1462bdc65523d0a1ec0a5380
aprilcao221 <97302206+aprilcao221@users.noreply.github.com>
2024-03-02 14:06:43 +0800

    updates

--- upgrade-blog/main.py
+++ upgrade-blog/main.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
+import smtplib
 
 app = Flask(__name__)
 blog_data = requests.get("https://api.npoint.io/4b100e1730f8ba42eea1").json()
@@ -10,18 +11,28 @@ def home():
 
     return render_template("index.html", posts=blog_data)
 
-@app.route("/about")
+@app.route("/about", methods=["POST", "GET"])
 def about():
-    return render_template("about.html")
+        return render_template("about.html")
 
-@app.route("/contact")
+@app.route("/contact", methods=["POST", "GET"])
 def contact():
+    if request.method == "POST":
+        form_data = request.form
+        with smtplib.SMTP("163.smtp.com", 25) as connection:
+            connection.starttls()
+            connection.login("caomancomeon@163.com", "psw")
+            connection.sendmail(from_addr="caomancomeon@163.com",
+                                to_addrs=form_data["email"],
+                                msg=form_data["message"])
+        return render_template("contact.html", received=True)
     return render_template("contact.html")
 
 @app.route("/post/<int:index>")
 def get_post(index):
     blog = blog_data[index-1]
     return render_template("post.html", post=blog)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
